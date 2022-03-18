7a80e873d029b4554bd149553be1adf9fbef4416
Irene del Castillo Croissier <91098775+irenecascro@users.noreply.github.com>
2022-03-18 10:54:45 +0000

    Add files via upload
    BREAKING BAD

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+/* librerias */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+/*Componentes */
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterListComponent,
+    CharacterCardComponent,
+    CharacterDetailComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
